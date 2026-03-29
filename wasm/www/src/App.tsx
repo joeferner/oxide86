006602f0b809e3cf75d7bf92c56f2966ff088b89
Joe Ferner <joe@fernsroth.com>
2026-03-29 15:45:08 -0400

    format

--- wasm/www/src/App.tsx
+++ wasm/www/src/App.tsx
@@ -7,12 +7,21 @@ import styles from './App.module.scss';
 export function App(): React.ReactElement {
     return (
         <div className={styles.page}>
-            <div className={styles.center}>
-                <div className={styles.screenStack}>
-                    <Screen />
-                    <StatusBar />
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <img src="/logo.png" alt="Oxide86" className={styles.logo} />
+                    <div>
+                        <h1 className={styles.title}>Oxide86</h1>
+                        <p className={styles.subtitle}>x86 Rust Emulator</p>
+                    </div>
                 </div>
-                <Toolbar />
+                <div className={styles.center}>
+                    <div className={styles.screenStack}>
+                        <Screen />
+                        <StatusBar />
+                    </div>
+                    <Toolbar />
+                </div>
             </div>
         </div>
     );
